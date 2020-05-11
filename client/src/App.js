03d0e214a480ed01a9cede948960ec497125c9b9
Melis Cagan <meliscagan@Meliss-MacBook-Air.local>
2020-05-11 13:01:53 -0400

    barebones of the sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "./SignUpForm.css";
+import SignUpContainer from "./components/SignUpContainer";
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <SignUpContainer />
     </div>
   );
 }
---
+++ client/src/components/SignUpForm.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+export default function SignUpForm(props) {
+  return (
+    <div className="loginBox">
+      <h1>Sign Up</h1>
+      <form onSubmit={props.onSubmit}>
+        <TextField
+          id="standard-secondary"
+          label="First Name"
+          color="secondary"
+          name="first name"
+          fullWidth={true}
+        />
+        <TextField
+          id="standard-secondary"
+          label="Last Name"
+          color="secondary"
+          name="last name"
+          fullWidth={true}
+        />
+        <TextField
+          id="standard-secondary"
+          label="Username"
+          color="secondary"
+          name="username"
+          fullWidth={true}
+        />
+        <TextField
+          id="standard-secondary"
+          label="Email"
+          color="secondary"
+          name="email"
+          fullWidth={true}
+        />
+        <TextField
+          id="standard-secondary"
+          label="Password"
+          color="secondary"
+          name="password"
+          fullWidth={true}
+        />
+        <TextField
+          id="standard-secondary"
+          label="Password Confirmation"
+          color="secondary"
+          name="password confirmation"
+          fullWidth={true}
+        />
+        <TextField
+          id="standard-secondary"
+          label="Location"
+          color="secondary"
+          name="location"
+          fullWidth={true}
+        />
+        <TextField
+          id="standard-secondary"
+          label="Occupation"
+          color="secondary"
+          name="occupation"
+          fullWidth={true}
+        />
+        <TextField
+          id="standard-secondary"
+          label="About Me"
+          color="secondary"
+          name="about me"
+          fullWidth={true}
+        />
+        <br />
+        <Button className="signUpSubmit" type="submit" label="submit" />
+      </form>
+      <p>
+        Aleady have an account? <br />
+        <a href="/login">Log in here</a>
+      </p>
+    </div>
+  );
+}
