01e0538caef296a1d846c3a523bc282cc9d077c5
Melis Cagan <meliscagan@Meliss-MacBook-Air.local>
2020-05-20 18:19:44 -0400

    increase shadow for homepage text

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -37,7 +37,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DB_URL,
     pool: {
       min: 2,
       max: 10,
